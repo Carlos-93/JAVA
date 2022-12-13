19613034ea106af99902b7dc0582d56efb1044bc
Carlos Araujo Galván <119673306+Carlos-93@users.noreply.github.com>
2022-12-14 00:16:11 +0100

    Add files via upload

---
+++ 1 DAW/TEMA 01 - HOLA MUNDO/HelloWorld.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package helloworld;
+
+/**
+ *      exercise 01 / compile a hello world on screen
+ * 
+ * @author Carlos
+ */
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World in Java with Carlos Araujo");
+    }
+}
